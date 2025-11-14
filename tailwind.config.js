a1a83657c459a762a36e70a89f92b9a0154963f0
Juseval <juseval98@hotmail.com>
2025-11-13 21:18:02 -0500

    cambio version tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lucius-lime': '#91A673',
+        'bright-white': '#F6F2F1',
+        'dark-hunter-green': '#3A4A3F',
+        'whisper-white': '#EAE2D3',
+        'bokara-grey': '#2A2725',
+        'wet-sand': '#AE8F60',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Orbitron', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
-
